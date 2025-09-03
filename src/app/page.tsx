55168561af87207e7d6be4e0e3f5635ab0fa5914
Kayode <dadavictory2000@gmail.com>
2025-09-03 10:26:48 +0100

    fix: fix gradient background on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gradient-to-tr from-[#8A3BEF]to-[#B457FA] flex justify-center items-center">
+    <div className="w-full h-screen bg-gradient-to-tr from-[#8A3BEF] to-[#B457FA] flex justify-center items-center">
       <h1 className="text-white font-bold text-4xl">Welcome to EventNest</h1>
       <Button
         className="bg-white text-[#8A3BEF] hover:bg-gray-100 absolute top-10 right-10"
